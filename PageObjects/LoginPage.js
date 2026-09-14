e4934e5e30aedaa80d71469cc7e0fdfdd23925b3
QA08 <QA08@gracetech.local>
2026-09-15 01:18:55 +0500

    update test

--- PageObjects/LoginPage.js
+++ PageObjects/LoginPage.js
@@ -1,12 +1,9 @@
 const { expect } = require('@playwright/test');
 const envConfig = require('../config/env.prod');
-
 class LoginPage {
-  
   constructor(page) {
     this.page = page;
   }
-
   // Target URL navigate karne ke liye
   async navigateToPage() {
     await this.page.goto(envConfig.baseURL, { 
